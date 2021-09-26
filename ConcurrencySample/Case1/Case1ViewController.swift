ec52b5d59cff036e032e140be4aaa1bada51eb73
seiya-sugioka <seiya_sugioka@waku-2.com>
2021-09-26 11:42:58 +0900

    case 1を追加

--- ConcurrencySample/Case1/Case1ViewController.swift
+++ ConcurrencySample/Case1/Case1ViewController.swift
@@ -10,7 +10,33 @@ import UIKit
 class Case1ViewController: UIViewController {
 
     override func viewDidLoad() {
+        print("--------- Case1ViewController ---------")
         super.viewDidLoad()
+        print("Before ready")
+        downloadDataBefore(from: URL(string: "https://")!) { data in
+            print(data)
+        }
+        
+        Task {
+            // print("After ready")とprint(data)が同じスレッドで実行される保証はない
+            print("After ready")
+            let data = await downloadDataAfter(from: URL(string: "https://")!)
+            print(data)
+        }
+    }
+}
+
+extension Case1ViewController {
+    func downloadDataBefore(from url: URL, completion: @escaping (String) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            completion("Before Data")
+        }
+    }
+    
+    func downloadDataAfter(from url: URL) async -> String {
+        /// 多分1秒遅延
+        await Task.sleep(1 * 1000 * 1000 * 1000)
+        return "After Data"
     }
 }
 
